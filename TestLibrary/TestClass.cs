3da6fca72f1a947729ac2921ab37d42e6ac38aee
hybri <hybridwebtech@gmail.com>
2023-10-19 11:20:43 -0600

    Added references to SQLite and Newtonsoft JSON

--- TestLibrary/TestClass.cs
+++ TestLibrary/TestClass.cs
@@ -1,6 +1,14 @@
-﻿namespace TestLibrary
+﻿using System.Data.SQLite;
+
+namespace TestLibrary
 {
     public class TestClass
     {
+        private SQLiteConnection _conn;
+
+        public TestClass()
+        {
+            
+        }
     }
 }
